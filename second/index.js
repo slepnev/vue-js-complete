6d2e41e054ed73e5894d08f874248bd4d4d844f9
ayslepnev <ayslepnev@domclick.ru>
2018-12-27 20:23:25 +0300

    Vue: init

--- second/index.js
+++ second/index.js
@@ -4,8 +4,18 @@ new Vue({
     title: 'Hello World!',
     title2: 'Hello World!',
     name: 'Alex',
+    name2: 'Max',
     link: 'https://google.com',
     finishedLink: '<a href="https://google.com">Google</a>',
+    counter: 0,
+    secondCounter: 0,
+    x: 0,
+    y: 0,
+  },
+  computed: {
+    output: function () {
+      return this.counter > 5 ? 'Greate 5' : 'Smaller 5';
+    }
   },
   methods: {
     changeTitle: function (event) {
@@ -14,6 +24,25 @@ new Vue({
     seyHello: function () {
       this.title = 'Hello!';
       return this.title;
+    },
+    increase: function (step, event) {
+      this.counter += step;
+    },
+    decrease: function () {
+      this.counter--;
+    },
+    updateCoordinates: function(event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    dummy: function (event) {
+      event.stopPropagation();
+    },
+    alertMe: function (event) {
+      alert('Alert!');
+    },
+    result: function() {
+      return this.counter > 5 ? 'Greate 5' : 'Smaller 5';
     }
   }
 });
