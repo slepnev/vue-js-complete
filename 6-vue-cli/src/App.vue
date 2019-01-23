1b60286bdc9faada3692aa679fdccf1915359bdc
ayslepnev <ayslepnev@domclick.ru>
2019-01-23 20:19:56 +0300

    Vue: vue cli

--- 6-vue-cli/src/App.vue
+++ 6-vue-cli/src/App.vue
@@ -1,13 +1,25 @@
 <template>
-  <div id="app">
-    <h1>Hello World!</h1>
+  <div>
+    <app-header></app-header>
+    <app-server></app-server>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+  import ServerStatus from './components/Server/ServerStatus.vue';
+  import Home from './components/Server/Home.vue';
+  import Header from './components/Shared/Header.vue';
+  import Footer from './components/Shared/Footer.vue';
+
+  export default {
+    components: {
+      'app-server-status': ServerStatus,
+      'app-server': Home,
+      'app-header': Header,
+      'app-footer': Footer,
+    }
+  }
 </script>
 
 <style lang="scss">
