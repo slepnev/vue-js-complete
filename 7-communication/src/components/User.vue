fef8ed8055403adbbe565b2686df8274b114be1f
ayslepnev <ayslepnev@domclick.ru>
2019-01-28 20:19:40 +0300

    Vue: props

--- 7-communication/src/components/User.vue
+++ 7-communication/src/components/User.vue
@@ -2,14 +2,20 @@
   <div class="component">
     <h1>The User Component</h1>
     <p>I'm an awesome User!</p>
+    <p>Name: {{ name }}</p>
     <button @click="changeName">Change my Name</button>
     <hr>
     <div class="row">
       <div class="col-xs-12 col-sm-6">
-        <app-user-detail :name="name"></app-user-detail>
+        <app-user-detail
+            :name="name"
+            @nameWasReset="name = $event"
+            :resetFn="resetName"
+            :userAge="age"
+        ></app-user-detail>
       </div>
       <div class="col-xs-12 col-sm-6">
-        <app-user-edit></app-user-edit>
+        <app-user-edit :userAge="age"></app-user-edit>
       </div>
     </div>
   </div>
@@ -22,12 +28,16 @@
   export default {
     data: function () {
       return {
-        name: 'Max'
+        name: 'Max',
+        age: 27
       };
     },
     methods: {
       changeName: function () {
         this.name = 'Anna';
+      },
+      resetName: function () {
+        this.name = 'Max';
       }
     },
     components: {
--- 7-communication/src/components/UserEdit.vue
+++ 7-communication/src/components/UserEdit.vue
@@ -2,10 +2,20 @@
     <div class="component">
         <h3>You may edit the User here</h3>
         <p>Edit me!</p>
+        <p>User Age: {{ userAge }}</p>
+        <button @click="editAge">Edit Age</button>
     </div>
 </template>
 
 <script>
+    export default {
+      props: ['userAge'],
+      methods: {
+        editAge: function () {
+          this.age = 30;
+        }
+      }
+    }
 </script>
 
 <style scoped>
