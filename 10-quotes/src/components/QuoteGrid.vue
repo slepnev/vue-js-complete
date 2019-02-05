6fecfe77efb576e177e496945b4603ae2bf01ecb
ayslepnev <ayslepnev@domclick.ru>
2019-02-05 20:22:14 +0300

    Vue: components

---
+++ 10-quotes/src/components/QuoteGrid.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row">
+    <quote v-for="(quote, i)  in quotes" :key="i"
+           @click.native="deleteQuote(i)">{{ quote }}</quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue';
+
+  export default {
+    name: 'QuoteGrid',
+    components: {Quote},
+    props: ['quotes'],
+    methods: {
+      deleteQuote(index) {
+        this.$emit('quoteDelete', index);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
