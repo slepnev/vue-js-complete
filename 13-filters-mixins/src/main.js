b5060cfb630f0ade86107ed089502be90119b0d1
ayslepnev <ayslepnev@domclick.ru>
2019-02-27 20:26:15 +0300

    Vue: mixins

--- 13-filters-mixins/src/main.js
+++ 13-filters-mixins/src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('to-lowercase', function (value) {
+  return value.toLowerCase();
+});
+
+Vue.mixin({
+  created() {
+    console.log('Global Mixin');
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
