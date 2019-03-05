d27065c0677ef941802b85a2268a73e2a1748b29
ayslepnev <ayslepnev@domclick.ru>
2019-03-05 20:10:22 +0300

    Vue: animations

---
+++ 15-animation-app/src/components/Answer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="alert alert-success text-center">
+    <h1>That's Correct!</h1>
+    <hr>
+    <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Answer',
+    methods: {
+      onNextQuestion() {
+        this.$emit('confirmed');
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
