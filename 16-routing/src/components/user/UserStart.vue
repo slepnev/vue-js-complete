10a19e4731ec27ac609d420422797bf31b24d1ca
ayslepnev <ayslepnev@domclick.ru>
2019-03-12 20:10:06 +0300

    Vue: routing

--- 16-routing/src/components/user/UserStart.vue
+++ 16-routing/src/components/user/UserStart.vue
@@ -1,11 +1,28 @@
 <template>
-    <div>
-        <p>Please select a User</p>
-        <hr>
-        <ul class="list-group">
-            <li class="list-group-item" style="cursor: pointer">User 1</li>
-            <li class="list-group-item" style="cursor: pointer">User 2</li>
-            <li class="list-group-item" style="cursor: pointer">User 3</li>
-        </ul>
-    </div>
+  <div>
+    <p>Please select a User</p>
+    <hr>
+    <ul class="list-group">
+      <router-link
+          v-for="(user, i) in users"
+          tag="li"
+          :to="`/user/${user}`"
+          class="list-group-item"
+          style="cursor: pointer"
+          :key="user"
+      >
+        User {{ user }}
+      </router-link>
+    </ul>
+  </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        users: [1, 2, 3, 4]
+      }
+    }
+  }
+</script>
